843d2a7c302b8e9f02082715e12598cd89ef4689
mercysound <adekunleadewolemercy@gmail.com>
2025-06-01 16:14:53 +0100

    idSellerPath using useLocation method in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
+
+  const isSellerPath = useLocation().pathname.includes("seller");
+
   return (
     <div>
-      
+      <Navbar/>
+      <div className='px-6 '>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+        </Routes>
+      </div>
     </div>
   )
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vite.d ev/config/
 export default defineConfig({
   plugins: [
     react(),
